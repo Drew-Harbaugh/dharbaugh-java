aead3b6610534765dada39573c58726788b0afe8
Drew Harbaugh <dharbaugh2290@gmail.com>
2020-10-29 00:11:26 -0400

    Week 7 day 2 exercise

--- module-2/13_ServerSide_APIs_Part_1/student-exercise/java/auctions/src/main/java/com/techelevator/auctions/controller/AuctionController.java
+++ module-2/13_ServerSide_APIs_Part_1/student-exercise/java/auctions/src/main/java/com/techelevator/auctions/controller/AuctionController.java
@@ -5,6 +5,9 @@ import com.techelevator.auctions.DAO.MemoryAuctionDAO;
 import com.techelevator.auctions.model.Auction;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @RestController
 public class AuctionController {
@@ -15,5 +18,37 @@ public class AuctionController {
         this.dao = new MemoryAuctionDAO();
     }
 
+//    @RequestMapping(path = "/auctions", method = RequestMethod.GET)
+//    public List<Auction> getAllAuctions() {
+//        return dao.list();
+//    }
 
+    @RequestMapping(path = "/auctions/{id}")
+    public Auction getAuction(@PathVariable int id) {
+        return dao.get(id);
+    }
+
+    @RequestMapping(path = "/auctions", method = RequestMethod.POST)
+    public Auction createAuction(@RequestBody Auction newAuction) {
+        return dao.create(newAuction);
+    }
+
+    @RequestMapping(path = "/auctions", method = RequestMethod.GET)
+    public List<Auction> searchAuctions(@RequestParam(defaultValue = "", name = "title_like") String title,
+                              @RequestParam(defaultValue = "0", name = "currentBid_lte") double currentBid) {
+        if (title.equals("") && currentBid != 0) {
+            return dao.searchByPrice(currentBid);
+        } else if (title.equals("") && currentBid == 0) {
+            return dao.list();
+        } else if (!title.equals("") && currentBid == 0) {
+            return dao.searchByTitle(title);
+        } else if (currentBid != 0 && !title.equals("")) {
+            return dao.searchByTitleAndPrice(title,currentBid);
+        }
+        return dao.list();
+    }
 }
+
+
+
+
